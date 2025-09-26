088b40d09420a1a7d8b1ed3a07214f5bfdac06e3
Adam McKellar <dev@mckellar.eu>
2025-09-27 00:37:37 +0200

    feat: added source and target arguments, added shell completions

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use clap::Parser;
+use std::path::PathBuf;
+
+use clap::{CommandFactory, Parser, ValueEnum, ValueHint};
+use clap_complete::Shell;
 use color_eyre::eyre::{Ok, Result};
 use license_fetcher::read_package_list_from_out_dir;
 
@@ -12,11 +15,29 @@ use license_fetcher::read_package_list_from_out_dir;
 #[derive(Parser, Debug)]
 #[command(version, about, author)]
 struct Cli {
+    /// Path to file to be backed up
+    #[arg(value_name = "FILE", value_hint = ValueHint::FilePath)]
+    source: Option<PathBuf>,
+
+    /// Path to folder to place backups in
+    ///
+    /// Please do not use the folder for anything else!
+    #[arg(value_name = "TARGET_FOLDER", value_hint = ValueHint::DirPath)]
+    target: Option<PathBuf>,
+
     /// Print licenses
     ///
     /// Print licenses of this project and all its dependencies
-    #[arg(long)]
-    pub licenses: bool,
+    #[arg(long, exclusive = true)]
+    licenses: bool,
+
+    /// List supported shells for shell completions
+    #[arg(long, exclusive = true)]
+    supported_shells: bool,
+
+    /// Print shell completion for requested shell
+    #[arg(long, exclusive = true, value_enum)]
+    generate_completion: Option<Shell>,
 }
 
 fn main() -> Result<()> {
@@ -27,6 +48,21 @@ fn main() -> Result<()> {
     if cli.licenses {
         let package_list = read_package_list_from_out_dir!()?;
         println!("{}", package_list);
+        return Ok(());
+    }
+
+    if cli.supported_shells {
+        for shell in Shell::value_variants() {
+            println!("{shell}");
+        }
+        return Ok(());
+    }
+
+    if let Some(shell) = cli.generate_completion {
+        let mut command = Cli::command();
+        let command_name = command.get_name().to_string();
+        eprintln!("Generating shell completions for {}", shell);
+        clap_complete::generate(shell, &mut command, command_name, &mut std::io::stdout());
         return Ok(());
     }
 
