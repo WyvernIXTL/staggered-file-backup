2f26cde3ded3fea6761dcc02cd6956153ff216cb
Adam McKellar <dev@mckellar.eu>
2025-09-30 22:54:00 +0200

    build: fix powershell pgo collection script failing on unix systems

--- scripts/collect-pgo-profiles.ps1
+++ scripts/collect-pgo-profiles.ps1
@@ -19,13 +19,13 @@ $content | Out-File -FilePath "./test/largefile.txt" -Encoding UTF8
 
 $env:LLVM_PROFILE_FILE="./target/pgo-profiles/$($bin)_%m_%p.profraw"
 
-& ./target/$TargetTriple/optimized/$bin .\test\largefile.txt .\test\target\
-& ./target/$TargetTriple/optimized/$bin .\test\largefile.txt .\test\target\
-& ./target/$TargetTriple/optimized/$bin .\test\largefile.txt .\test\target\
+& ./target/$TargetTriple/optimized/$bin ./test/largefile.txt ./test/target/
+& ./target/$TargetTriple/optimized/$bin ./test/largefile.txt ./test/target/
+& ./target/$TargetTriple/optimized/$bin ./test/largefile.txt ./test/target/
 & ./target/$TargetTriple/optimized/$bin --help
 & ./target/$TargetTriple/optimized/$bin --licenses
-& ./target/$TargetTriple/optimized/$bin -n 3 -m 2 .\test\largefile.txt .\test\target\
-& ./target/$TargetTriple/optimized/$bin -y 1 -d 1 .\test\largefile.txt .\test\target\
+& ./target/$TargetTriple/optimized/$bin -n 3 -m 2 ./test/largefile.txt ./test/target/
+& ./target/$TargetTriple/optimized/$bin -y 1 -d 1 ./test/largefile.txt ./test/target/
 
 # Compile optimized:
 # cargo pgo optimize build -- --profile optimized --target x86_64-pc-windows-msvc
