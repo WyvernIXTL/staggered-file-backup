98678ad559758bbeb6ac62a19ee2b6f25b9ccfa5
Adam McKellar <dev@mckellar.eu>
2025-10-01 13:19:06 +0200

    feat: add migration to v1 db

---
+++ migrations/2025-10-01-111135-0000_v1/up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE backup_files (
+  uuid BLOB NOT NULL PRIMARY KEY,
+  relative_path BLOB NOT NULL,
+  keep_yearly INTEGER NOT NULL,
+  keep_monthly INTEGER NOT NULL,
+  keep_daily INTEGER NOT NULL,
+  keep_latest INTEGER NOT NULL,
+)
